15381b7d60b03cb0d758a56df5fef76b5d5012c3
NANDY1211 <nandhakishore1211@gmail.com>
2026-03-02 13:59:37 +0530

    timer added

--- simple_programs/lambda.cpp
+++ simple_programs/lambda.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <math.h>
+#include <chrono>
+
+
+struct Timer
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> start,end;
+    std::chrono::duration<float> duration;
+
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+    
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+        duration = end-start;
+        float ms = duration.count() * 1000.0f;
+        std::cout<<"\nTimer took "<< ms <<" ms"<<"\n";
+    }
+
+};
 
 auto addRes = [](int val1, int val2)
 {
+    Timer t;
     return val1 + val2;
 };
 
